f2e15675e7832dcea741636db4fc1ec5adcfd58e
kevinshawn102103 <jaycejames1021@gmail.com>
2017-01-22 17:23:10 -0600

    add java script extension
    add on

---
+++ java script.js
@@ -0,0 +1,36 @@
+var health=100;
+checkhealth(health);
+health=health+1;
+health=health+3;
+health=health-5;
+health=health-99;
+checkhealth(health);
+
+
+
+
+fuction check health(healthtocheck)
+{	if(healthtocheck == 0)
+{alert("you have died must go to last check point");
+	
+
+}
+	
+
+}
+var power=100
+checkpower(power);
+power=power+1;
+power=power+3;
+power=power-5;
+power=power-99;
+checkpower(power);
+
+
+fuction check power(powertocheck)
+{    if (powertocheck== 0)
+{alert("you can not Do alot of damage go find more power");
+
+
+
+]
